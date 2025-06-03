c4eeb0b84089d4791e5847baf1e6cfe78e0e347f
Nhrgetic <ninomajstor@gmail.com>
2025-06-03 19:20:09 +0200

    Add files via upload

---
+++ LV6/6_1.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import datasets
+
+def generiraj_skup_podataka(broj_uzoraka, nacin):
+    if nacin == 1:
+        stanje = 365
+        podaci, _ = datasets.make_blobs(n_samples=broj_uzoraka, random_state=stanje)
+    elif nacin == 2:
+        stanje = 148
+        podaci, _ = datasets.make_blobs(n_samples=broj_uzoraka, random_state=stanje)
+        matrica = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
+        podaci = np.dot(podaci, matrica)
+    elif nacin == 3:
+        stanje = 148
+        podaci, _ = datasets.make_blobs(n_samples=broj_uzoraka, centers=4,
+                                        cluster_std=[1.0, 2.5, 0.5, 3.0], random_state=stanje)
+    elif nacin == 4:
+        podaci, _ = datasets.make_circles(n_samples=broj_uzoraka, factor=0.5, noise=0.05)
+    elif nacin == 5:
+        podaci, _ = datasets.make_moons(n_samples=broj_uzoraka, noise=0.05)
+    else:
+        podaci = np.array([])
+    return podaci
+
+ulazni_podaci = generiraj_skup_podataka(500, nacin=4)
+
+plt.figure(figsize=(6, 5))
+plt.scatter(ulazni_podaci[:, 0], ulazni_podaci[:, 1], s=15)
+plt.title("Generirani podaci")
+plt.xlabel("Znacajka 1")
+plt.ylabel("Znacajka 2")
+plt.grid(True)
+plt.show()
+
+broj_klastera = 3
+kmeans_model = KMeans(n_clusters=broj_klastera, random_state=0)
+pripadnosti = kmeans_model.fit_predict(ulazni_podaci)
+sredisnje_tocke = kmeans_model.cluster_centers_
+
+plt.figure(figsize=(6, 5))
+plt.scatter(ulazni_podaci[:, 0], ulazni_podaci[:, 1], c=pripadnosti, s=15, cmap='viridis')
+plt.scatter(sredisnje_tocke[:, 0], sredisnje_tocke[:, 1], c='black', s=180, marker='X', label='Centri klastera')
+plt.title("Rezultat grupiranja s KMeans algoritmom")
+plt.xlabel("Znacajka 1")
+plt.ylabel("Znacajka 2")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Ako promijenimo način na koji se generiraju podaci dobit ćemo drugi oblik na grafu
